0a138283bd7b383b84bb7994a75f223fe3d9ab69
leejinseong9410 <81233734+leejinseong9410@users.noreply.github.com>
2024-04-01 14:15:53 +0900

    description 수정

--- public/seo.config.tsx
+++ public/seo.config.tsx
@@ -13,7 +13,7 @@ function SEO({ title, description, image }: SEOProps) {
       description={
         description
           ? description
-          : 'LTechUVC는 그린수소, 그린 암모니아 기반 스마트 시티 전문 기업입니다'
+          : '그린수소 그린암모니아 기반 스마트시티 전문기업, 엘텍유브이씨'
       } // 50~160자 이하
       canonical="https://www.ltk-uvc.com"
       openGraph={{
@@ -23,14 +23,14 @@ function SEO({ title, description, image }: SEOProps) {
         title: title ? `${title} | 엘텍유브이씨, LTechUVC` : '엘텍유브이씨, LTechUVC',
         description: description
           ? description
-          : 'LTechUVC는 그린수소, 그린 암모니아 기반 스마트 시티 전문 기업입니다',
+          : '그린수소 그린암모니아 기반 스마트시티 전문기업, 엘텍유브이씨',
         site_name: '엘텍유브이씨, LTechUVC',
         images: [
           {
             url: image
               ? image
               : 'https://res.cloudinary.com/dp0gh7jel/image/upload/v1700796847/iPhone_14_Pro_1_kax2ix.png',
-            alt: 'LTechUVC는 그린수소, 그린 암모니아 기반 스마트 시티 전문 기업입니다',
+            alt: '그린수소 그린암모니아 기반 스마트시티 전문기업, 엘텍유브이씨',
           },
         ], // 16:9 , 1200px 이하
       }}
